ee8dfbed2c76daf78c4159733c601a36475e86f3
bacembenhfayehd <bacem.benhfayedh@gmail.com>
2025-07-03 10:56:51 +0100

    cartpage

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useCart } from '../context/CartContext'
 
 const CartPage = () => {
+  const {cart, removeFromCart} = useCart();
+
+  const total  = cart.reduce((sum,item) => sum + item.product.price * item.quantity,0) 
   return (
-    <div>CartPage</div>
+    <div>
+      <h2>Panier</h2>
+      {cart.map(({product,quantity}) => (
+        <div key={product.id}>
+          {product.title} - {quantity} * {product.price} TND
+ <button onClick={() => removeFromCart(product.id)}>supprimer</button>
+        </div>
+      ))}
+
+      <h3>total : {total}</h3>
+    </div>
   )
 }
 
